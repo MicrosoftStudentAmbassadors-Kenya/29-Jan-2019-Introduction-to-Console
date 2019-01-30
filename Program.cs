343651eebe8053acafe34a7d3273f439ebd56adb
Humphryshikunzi <39647238+Humphryshikunzi@users.noreply.github.com>
2019-01-30 20:30:37 +0300

    Add files via upload
    The first console commands for the second group for Microsoft Tech Community-Dekut Club

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("charity");
+            Console.Write("2019");
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("visionn2030");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("world investment");
+            Console.Title = "financial risk";
+            Console.Clear();
+            Console.WriteLine("enter your name");
+
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("password");
+            string password = Console.ReadLine();
+            Console.WriteLine(password);
+            Console.Clear();
+            Console.WriteLine(name);
+            Console.WriteLine(password);
+            Console.ReadKey();
+         }
+        
+    }
+    
+}
